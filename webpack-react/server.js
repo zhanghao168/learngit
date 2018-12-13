a645f16b5dde072342cf5326acf13a3ba0c21eea
zhanghao168 <623716242@qq.com>
2018-12-13 16:42:50 +0800

    add webpack-react

---
+++ webpack-react/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config.js');
+const compiler = webpack(config);
+const app = express();
+const port = 3000;
+
+// Tell express to use the webpack-dev-middleware and use the webpack.config.js
+// configuration file as a base.
+app.use(webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    stats: {
+        colors: true
+    }
+}));
+app.use(webpackHotMiddleware(compiler));
+
+
+app.get('/', function(req, res) {
+    res.sendfile('./dist/index.html');
+});
+
+
+app.listen(port, (error) => {
+    if (error) {
+        console.error(error)
+    } else {
+        console.info(`==> 🌎  Listening on port ${port}. Open up http://localhost:${port}/ in your browser.`)
+    }
+})
---
+++ webpack-react/src/index.js
@@ -0,0 +1,17 @@
+import { createStore } from 'redux';
+import todoApp from './reducers';
+import { addTodo, toggleTodo, setVisibilityFilter, VisibilityFilters } from './actions';
+const store = createStore(todoApp);
+
+console.log(store.getState());
+
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(addTodo('Learn about actions'));
+store.dispatch(addTodo('Learn about reducers'));
+store.dispatch(addTodo('Learn about store'));
+store.dispatch(toggleTodo(0));
+store.dispatch(toggleTodo(1));
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
+
+unsubscribe();
